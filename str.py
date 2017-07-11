c9e2f516e231622ad2a9109cf8f4ec02d80b9469
sunnyliu <1322970235@qq.com>
2017-07-11 16:15:54 +0800

    Update str.py

--- str.py
+++ str.py
@@ -1,1 +1,21 @@
 print("hello world")
+>>> a = {1,2,3,4}
+>>> b ={3,4,5,6}
+>>> a
+{1, 2, 3, 4}
+>>> type(a)
+<class 'set'>
+>>> a.symmetric_difference(b)
+{1, 2, 5, 6}
+>>> b.symmetric_difference(a)
+{1, 2, 5, 6}
+>>>
+>>>
+>>> a.difference(b)
+{1, 2}
+>>> a.union(b)
+{1, 2, 3, 4, 5, 6}
+>>> a.issu
+a.issubset(   a.issuperset(
+>>> a.issubset(b)
+False
